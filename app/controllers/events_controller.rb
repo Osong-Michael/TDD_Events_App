a5051df3aeacd61c177bab9b22e47881386cbee7
Osong Agberndifor <agberndifor7@gmail.com>
2020-06-20 22:50:43 +0100

    Created form to update events from the webpage

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,4 +6,18 @@ class EventsController < ApplicationController
   def show
     @event = Event.find(params[:id])
   end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    event_params = params.require(:event).permit(:name, :description, :location, :price, :starts_at)
+
+    @event.update(event_params)
+    
+    redirect_to event_path(@event)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root 'events#index'
   get 'events' => 'events#index'
   get 'events/:id' => 'events#show', as: 'event'
+  get 'events/:id/edit' => 'events#edit', as: 'edit_event'
+  patch 'events/:id' => 'events#update'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
