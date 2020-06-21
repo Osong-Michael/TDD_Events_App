767ef75b74b6d932ff4708de7424d04769095b29
Osong Agberndifor <agberndifor7@gmail.com>
2020-06-21 09:34:55 +0100

    Add view to create new event on web interface and actions to handle creating new events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -13,11 +13,27 @@ class EventsController < ApplicationController
 
   def update
     @event = Event.find(params[:id])
-    event_params = params.require(:event).permit(:name, :description, :location, :price, :starts_at)
 
     @event.update(event_params)
     
     redirect_to event_path(@event)
+  end
+
+  def new 
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.save 
+
+    redirect_to event_path(@event)
+  end
+  
+  private 
+
+  def event_params
+    params.require(:event).permit(:name, :description, :location, :price, :starts_at)
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   root 'events#index'
-  get 'events' => 'events#index'
-  get 'events/:id' => 'events#show', as: 'event'
-  get 'events/:id/edit' => 'events#edit', as: 'edit_event'
-  patch 'events/:id' => 'events#update'
+  # get 'events' => 'events#index'
+  # get 'events/new' => 'events#new'
+  # get 'events/:id' => 'events#show', as: 'event'
+  # get 'events/:id/edit' => 'events#edit', as: 'edit_event'
+  # patch 'events/:id' => 'events#update'
+  resources :events
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
